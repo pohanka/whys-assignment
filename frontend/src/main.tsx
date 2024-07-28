caf8ac17abb7b27d1769c62a45b7aca2e7e01f05
Klaudia Pohankova <xpohank@fi.muni.cz>
2024-07-28 17:21:07 +0200

    project init

---
+++ frontend/src/main.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  breakpoints: {
+    mobile: 0,
+    desktop: "768px",
+  },
+  colors: {
+    primary: "#83C5BE",
+    secondary: "#006D77",
+    backgroundColor: "#EDF6F9",
+    info: "#333333",
+    shadow: "#959595",
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+);
